756cf2d5f410dd50549180ab10adf27ff5070e49
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-13 21:36:49 +0530

    Initial commit

---
+++ python/Input.py
@@ -0,0 +1,23 @@
+#Input
+
+# formatted string
+
+name="priyanka"
+age=20
+
+print(f"my name is {name} and my age is {age}")
+
+# Row Strings
+
+
+name=input("what is your name?")
+
+print(name)
+
+number=input('Number is')
+
+print(number)
+
+age=input("What is your age?")
+
+print(age)
---
+++ python/test.py
@@ -0,0 +1,100 @@
+print("hello Word")
+
+# Comments
+# that are ignored by the python
+# Doc-strings use for multi-line comments ""
+
+# Variables 
+# variable are used to store the variables
+
+# ***Rules*******
+# you can't use number at the variables start
+# you can't use space in vairables
+# you should not use special character in variables
+sher="cheetah";
+age=20
+
+print(sher)
+print(age)
+ 
+SheryianSchool="Students" #pascal case
+sheryianSchool="yeyyee" #camel case
+  
+sheryian_school="students"  #snake case
+
+
+#Data Types
+"""
+integer
+boolean
+float
+fraction
+character
+strings                  
+
+"""
+
+fees=20000;
+v=34j #it is used to represent binary number
+
+print(type(fees))
+
+
+# Strings
+
+a="A"
+
+print(ord(a)) #it convert the  characteer into unicode
+a=65
+
+print(chr(a)) #it convert the unicode into characteer
+
+
+# String indexing
+
+""" start from 0 
+
+"""
+
+name="Priyanka rawat"
+
+print(name[2]) #i
+
+# string Slicing 
+
+#to cut out the portion
+
+#string[start : stop : step]
+#print(name[0:4:1]) #Priy
+
+print(name[9:14])
+
+
+#Type Conversion
+
+b=12
+
+b=str(b)
+
+print(type(b))
+
+
+c="hello"
+
+c=bool(c)
+
+print(type(c))
+
+
+#Falsy values
+
+"""
+false
+0
+0.0
+""
+[]
+()
+{}
+
+"""
