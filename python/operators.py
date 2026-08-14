9b837a715954b00aeacbe04733fc55e2ec5cc19d
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-14 21:41:56 +0530

    operators

---
+++ python/operators.py
@@ -0,0 +1,81 @@
+# Operators
+"""
+python follows bodmas rules
+"""
+
+# 1************Artmetic Operation*********
+# a=5
+# b=20
+
+# print(a+b)
+# print(b-a)
+# print(a*b)
+
+# print(a%b)
+
+# # Float Divison //
+
+# # / vs //
+
+# print(b//a) # 4 values converted into integers
+
+# print(b/a) #4.0
+
+# print(5**2) #5sq 5x5
+# print(5**3) # 5X5X5
+
+# print(5**100)
+
+# print(32%5) #Remainder
+
+
+
+#*********Assignment operators************
+
+#(=,+=,-=,/=,**=)
+#used to assgin the values to variable
+
+
+a=20
+# a=a+40
+# a=a+60
+
+
+#Compound Assign Operations
+
+# a+=40
+# a-=20
+
+# print(a)
+
+
+# Comparision Operator
+#(==, ===,!=,> ,<, >=,<=)
+
+#used to campare more then two variables, 
+# always provide boolean variables
+
+# b=20
+# c=30
+# print(b==c)
+
+# print(b!=c)
+
+# #we can also compare float and integer variables
+
+# print(ord("A")) #65
+# print(ord("B")) #66
+
+# print("A" > "B")
+
+#Logical Operators 
+
+#(and,or , !=)
+
+print(34!=35)
+
+
+
+
+
+
