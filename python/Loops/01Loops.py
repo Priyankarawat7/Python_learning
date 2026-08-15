c478cac15c75cef512297a85181c044d17c0d93c
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-15 13:39:34 +0530

    add loops

---
+++ python/Loops/01Loops.py
@@ -0,0 +1,93 @@
+                  
+#Loops
+"""
+While Loop,For loop
+(repeatition)
+
+for loops is based on range on the other
+half while loop is based on the condition
+
+
+For Loops
+#range Function
+
+range(start,stop,steps)
+"""
+
+#No need to give all three values to range 
+# you can also start the range in reverse order 
+#a=range(1,20,1)
+
+# for i in a:
+#     print(i) #1 to 20  
+
+
+# for i in range(1,30,1):
+#     print(i)
+
+
+# for i in range(16,1,-1):
+#     print(i) #16 to 2
+
+# for i in range(-5,-16,-1):
+#     print(i) #-5 to -15
+
+
+#5 table
+
+# n=int(input("Which table you want to print:-"))
+
+# for i in range(n,n*10+1,n):
+#     print(i) 
+
+# a="SHERYIANS TEACHERS INDUSRTY THINGS"
+
+# print(len(a))
+# for i in range(len(a)):
+#      print(a[i])
+
+# b="Sheryians is cool"
+
+# for i in b:
+#      print(i) 
+
+
+#Breaks,Continue
+#else will works with break
+
+#agr break chla to else nhi chlega agr else clha to break nhi chlega
+
+# for i in range(1,21):
+#    if i==6:
+#      break 
+#    else:
+#      print(i)
+                  
+
+# for i in range(1,25):
+#   if i==9: # 9 will skip   
+
+#     continue
+#   else:
+#     print(i)
+
+
+
+
+for i in range(1,30):
+  if i==10:
+    print("break is executed")
+    break
+    print(i)
+  else:
+    print("Break is not executed")
+
+
+
+
+
+
+
+
+
+
