5fa9bef66ffed60c2fef517c03a59d41169331b0
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-18 19:57:06 +0530

    add list game

---
+++ python/DSA/01dsa.py
@@ -0,0 +1,37 @@
+# to structured the data called data-structures
+
+"""
+List Powers
+
+
+Mutable ->change krna ksi bhi value ko
+Duplicates->Multiple same value
+Ordered-> Always this list are ordered
+Heterogenous ->
+"""
+a=[12,13,14,15,15,5.5,True,print()] #you can store multiple data
+
+print(a)
+
+
+"""
+List basics
+
+String indexing,slicing
+
+a="hello"
+print(a[0]) indexing
+print(a[::])
+
+you can do in list also there is no diffrence at all
+"""
+
+
+print(a[2:6:1])
+
+
+"""
+List Traversing
+"""
+
+                    
