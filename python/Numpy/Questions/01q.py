bc0f9b276c7b897cde15b4ad02378101f17ce713
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-09-06 23:27:19 +0530

    add questions

---
+++ python/Numpy/Questions/01q.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+#Valid Sudoku
+
+s=np.array([
+
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+
+
+])
+
+#Column axis=1
+#Row axis=0
+
+b=np.sum(s,axis=1)
+
+for i in b:
+    if i!=45:
+        print("Sudoku is not valid")
+        break
+else:
+        print("For rows it's valid")
+
+#print(b)
+
+p=np.sum(s,axis=0)
+
+for i in b:
+    if i!=45:
+        print("Sudoku is not valid")
+        break
+else:
+        print("For Column it's valid")
+
+
+
+
+
+#print(s[0:3,0:3])
+
+#print(s[0:3,3:6])
+
+for i in range(0,9,3):
+     for j in range(0,9,3):
+          n=s[i:i+3,j:j+3]
+         
+          print(n.sum())
+
